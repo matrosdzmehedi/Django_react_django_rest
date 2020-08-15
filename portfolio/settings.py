3a81537d713c64b67fec0ff09da8d1e443b6e900
mehedi <matrosdzmehedi>
2020-08-15 11:24:08 +0600

    added postgres

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+
 import dj_database_url
 from pathlib import Path
 import os
@@ -80,30 +81,32 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+# 'default': {
+#   'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dadah806u32s9e',
+        'USER': 'chobqdjblmucnh',
+        'PASSWORD': 'abe735526375e0d9425ae96d0a8c54aa63ef72920e927ad530b58f27436754cb',
+        'HOST': 'ec2-184-72-162-198.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600)
+
+db_from_env = dj_database_url.config(
+    'postgres://chobqdjblmucnh:abe735526375e0d9425ae96d0a8c54aa63ef72920e927ad530b58f27436754cb@ec2-184-72-162-198.compute-1.amazonaws.com:5432/dadah806u32s9e', conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 # DATABASES['default'] = dj_database_url.config(
 #   'postgres://chobqdjblmucnh:abe735526375e0d9425ae96d0a8c54aa63ef72920e927ad530b58f27436754cb@ec2-184-72-162-198.compute-1.amazonaws.com:5432/dadah806u32s9e', conn_max_age=600)
 
-
-# DATABASES = {
-#   'default': {
-#       'ENGINE': 'django.db.backends.postgresql',
-#       'NAME': 'dadah806u32s9e',
-#       'USER': 'chobqdjblmucnh',
-#       'PASSWORD': 'abe735526375e0d9425ae96d0a8c54aa63ef72920e927ad530b58f27436754cb',
-#       'HOST': 'ec2-184-72-162-198.compute-1.amazonaws.com',
-#       'PORT': '5432',
-#   }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
