e782086994ff00fe47993c14936b1d0191b30f19
Matrosdzmehedi <matrosdzmehedi111@gmail.com>
2020-08-26 15:14:19 +0600

    Update urls.py
    routing fix with django

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include,re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('account.urls')),
     path('', include('frontend.urls')),
+    re_path(r'blog/', include('frontend.urls')),
+    re_path(r'project/', include('frontend.urls')),
+    re_path(r'portfolio/', include('frontend.urls')),
+    re_path(r'contact/', include('frontend.urls')),
     path('api/', include('blog.urls')),
+    path('api/', include('account.urls')),
 ]
